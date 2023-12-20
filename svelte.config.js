7430129f27a1b592d9c764928000befd3640cdfe
Shazeb Ata <shazebata@Shazebs-Air.domain.name>
2023-12-20 14:26:08 +0530

    correct base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const base = '/slick-portfolio-svelte';
+const base = '/myspace';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
